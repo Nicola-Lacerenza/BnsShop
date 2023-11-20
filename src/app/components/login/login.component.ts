c42df6ba0b5881abf7bef5049a3c10a3d44bf35a
Nicola Lacerenza <niclaocell nicolace98@gmail.comgit config --global user.email niclaocell>
2023-11-20 09:46:27 +0100

    Update 20/11

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -10,7 +10,9 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent implements OnInit {
   autenticato: Boolean=false;
+  consentito : Boolean= false;
   errMsg : string = "Spiacente,la userid e/o la password sono errati!";
+  okMsg : string = "Accesso consentito";
 /*constructor(private router:Router){
 
 }*/
@@ -19,20 +21,31 @@ ngOnInit(): void {
 
 }
 
+
+
+
+
+
 public dologin(form:NgForm) : void {
 
 
-if (form.valid){
+/*if (form.valid){*/
   if (form.value.userId=="pippo" && form.value.password=="password"){
     this.autenticato = true
+    this.consentito = true
   }
     else
     this.autenticato = false
+    this.consentito = false
 
     /*AuthService.setLogin();
     this.router.navigate(["paginaPrivata"])*/
   }
+/*else{
+  this.autenticato = false
+  this.consentito = false
 }
+}*/
 /*public logout(): void{
   AuthService.logout();
   this.router.navigate(["homePage"])
